2ebf67f28df63cfa313e141bafa7a601cb700e47
Aylin Yay <aylinyay@outlook.com>
2023-03-28 21:07:24 +0300

    Add project files.

---
+++ Enum-Ornek/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            }
+            else if (sicaklik>=(int)HavaDurumu.Normal && sicaklik<(int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hadi dışarıya çıkalım!");
+            }
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal= 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
